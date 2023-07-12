381ddd8ab0dc0d363acc4a01ff453f44aa2835fa
Andy <andy@MacBook-Pro-di-Andy.local>
2023-07-12 18:25:40 +0200

    feat:Dashboard/page

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { auth } from "../auth";
 
 export default function Dashboard() {
   const navigate = useNavigate();
@@ -13,8 +14,8 @@ export default function Dashboard() {
   return isLogged ? (
     <div className="Dashboard">
       <h1>Dashboard</h1>
-      <p>Nome</p>
-      <p>Age</p>
+      <p>Nome: {auth.username}</p>
+      <p>Age: {auth.age}</p>
     </div>
   ) : (
     navigate("/login")
